3bf09239ffcd52c651302ddd9899ed858e31309c
Cherina97 <Cherina97@gmail.com>
2019-10-15 14:31:18 +0300

    my firts commit

---
+++ Task1.java
@@ -0,0 +1,27 @@
+public class Task1 {
+    public static void main(String[] args) {
+        byte n1 = 5;
+        System.out.println("Byte is " + n1);
+
+        short n2 = 258;
+        System.out.println("Short is " + n2);
+
+        int n3 = 58;
+        System.out.println("Int is " + n3 );
+
+        long n4 = 78569;
+        System.out.println("Long is " +n4);
+
+        char n5 = 'b';
+        System.out.println("Char is " + n5);
+
+        float n7 = 2.36f;
+        System.out.println("Float is " + n7);
+
+        double n8 = 56.36;
+        System.out.println("Double is " + n8 );
+
+
+
+    }
+}
